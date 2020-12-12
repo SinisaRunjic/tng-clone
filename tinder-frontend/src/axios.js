e03c1dc6f8fc075464a4937d09ef18b836069bfb
SinisaRunjic <sinisa.runjic@gmail.com>
2020-12-12 17:27:36 +0100

    baseURL change

--- tinder-frontend/src/axios.js
+++ tinder-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8001',
+    baseURL: (process.env.PORT) ? `${process.env.PORT}` : 'http://localhost:8001',
 })
 
 export default instance;
